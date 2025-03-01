c81688f4958949fe5f96a27581a4e1f512f60088
amad zarak <amadzarak16@gmail.com>
2025-03-01 13:01:11 -0500

    [MASTER]: Add TimeBucket class.

---
+++ TimeBucket.cs
@@ -0,0 +1,23 @@
+﻿
+namespace SchedulingCore;
+
+public class TimeBucket
+{
+    public Guid Id { get; set; }
+    public DateTime Start { get; set; }
+    public DateTime End { get; set; }
+    public TimeSpan Capacity { get; set; }
+    public TimeSpan UsedCapacity { get; set; }
+    public TimeSpan RemainingCapacity => Capacity - UsedCapacity;
+    
+    // NAVIGATION PROPERTIES
+    public virtual ICollection<ScheduledActivity> Activities { get; set; }
+
+    public bool CanFit(TimeSpan duration)
+        => true;
+
+    public void Assign(ScheduledActivity activity) { }
+    public void UnAssign(ScheduledActivity activity) { }
+}
+
+
